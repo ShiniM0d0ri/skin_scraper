4657715787661e160377c3407fc1d76a1fafa5c8
Vinay <vnchoudhary357@gmail.com>
2020-10-09 13:20:02 +0530

    fixed firefox process not closing(only for windows)

--- skin_scraper.py
+++ skin_scraper.py
@@ -1,4 +1,4 @@
-import time
+import time,os
 import json,requests,re
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -10,10 +10,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
 def setup_driver():
+    profile = webdriver.FirefoxProfile()
+    # 1 - Allow all images
+    # 2 - Block all images
+    # 3 - Block 3rd party images 
+    profile.set_preference("permissions.default.image", 2)
     options = Options()
-    options.headless = True
+    options.add_argument('-headless')
     driver = webdriver.Firefox(options=options)
     driver.get("https://csgoempire.com/withdraw#730")
     driver.set_window_size(942, 695)
@@ -35,14 +39,13 @@ def get_csgoempire(driver):
     except:
         print("Nothing listed right now")
     get_csgoempire_withdraw_items(driver)
-
     return driver
 
 def get_csgoempire_withdraw_items(driver):
     wear = driver.find_elements(By.XPATH, "//div[@class='item__head flex items-end']")
     name = driver.find_elements(By.XPATH, "//div[@class='px-2']")
     price = driver.find_elements(By.XPATH, "//span[@class='text-xxs font-bold text-light-grey-1']")
-    #get_steam(weapon,name,wear)
+    print(len(name))
     for i in range(0,len(name)):
         try:
             wear1=wear[i].text.title()
@@ -50,7 +53,7 @@ def get_csgoempire_withdraw_items(driver):
             continue
         try:
             steam_price=float(get_steam(name[i].text,wear1)['lowest_price'].strip('$'))
-        except (KeyError,TypeError):
+        except (KeyError,TypeError,IndexError):
             continue        
         em_coins=float(price[i].text)
         if(profit(em_coins,steam_price)):
@@ -60,15 +63,14 @@ def get_csgoempire_withdraw_items(driver):
         except IndexError:
             fv=0
         if (fv!=0 and gud_float(fv)):
-            print("This float is good: ", fv)
-    driver.refresh()
+            print(name[i].text.replace("\n"," "),"This float is good: ", fv)
 
 
 def get_steam_weapon(weapon,name,wear):
-    print(weapon,name,wear)
+    #print(weapon,name,wear)
     json_link="https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name="+weapon+"%20%7C%20"+name+"%20%28"+wear+"%29&currency=1"
     json_link=json_link.replace(" ","%20")
-    print(json_link)
+    #print(json_link)
     data=requests.get(json_link).json()
     return data
 
@@ -79,7 +81,7 @@ def get_steam_knife(weapon,name,wear):
     return data
 
 def get_steam_pin(name):
-    print(name)
+    #print(name)
     json_link="http://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name="+name+"&currency=1"
     json_link=json_link.replace(" ","%20")
     data=requests.get(json_link).json()
@@ -90,7 +92,7 @@ def get_steam_sticker(name,tour,qual):
         qual="%20%28"+qual+"%29%20"
     if tour!="":
         tour="%7C"+tour
-    print(name,qual,tour)
+    #print(name,qual,tour)
     json_link="http://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=Sticker%20%7C%20"+name+qual+tour+"&currency=1"
     json_link=json_link.replace(" ","%20")
     data=requests.get(json_link).json()
@@ -125,8 +127,9 @@ def gud_float(fv):
     if 0.20>fv>0.15 or 0.07<fv<0.11 or fv<0.05:
         return True
 
-def profit(em_coins,steam_price):
-    if em_coins<steam_price:
+def profit(em,steam):
+    if ((em-steam)/em)<=0.04:
+        os.system("powershell -c echo `a")
         return True
 
 
@@ -135,13 +138,31 @@ def profit(em_coins,steam_price):
 
 def main():
     driver=setup_driver()
+    run_time="1"
+    run_time_cond=input("If you want to run the script continuously type 'yes'and press enter: ")
+    if(run_time_cond=="yes"):
+        run_time="0"
     try:
         while(True):
-            get_csgoempire(driver)
-            time.sleep(5)
+            os.system("cls")
+            driver=get_csgoempire(driver)
+            print("refreshing...")
+            time.sleep(3)
+            print("run_time: ",run_time)
+            if(run_time=="1"):
+                break
+            driver.refresh()
     except KeyboardInterrupt:
+        print("Closing the script\nMight take some time so..\nDON'T PRESS IT AGAIN")
         driver.quit()
-    finally:
-        driver.quit()
+        print("closing remaining firefox process")
+        os.system("pause")
+        os.system("""taskkill /f /im "firefox.exe" /t""")
+        exit(0)
+
+    driver.quit()
+    os.system("""taskkill /f /im "firefox.exe" /t""")
+
+    
 
 main()
